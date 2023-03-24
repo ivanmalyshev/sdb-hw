e9a187ba1b753275dedcc4d31731edffa9ea68ad
Ivan Malyshev <ivanmalyshev1994@gmail.com>
2023-03-24 21:42:58 +0300

    11-04 base done

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.88.238'))
+credentials = pika.PlainCredentials('mid', 'midx11011')
+#connection = pika.BlockingConnection(pika.ConnectionParameters('rmq1', 5672, '/', credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('rmq2', 5672, '/', credentials))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
@@ -11,5 +12,7 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+#channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume('hello', callback, auto_ack=True)
 channel.start_consuming()
+
--- 11-04/producer.py
+++ 11-04/producer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+credentials = pika.PlainCredentials('mid', 'midx11011')
+#connection = pika.BlockingConnection(pika.ConnectionParameters('rmq1', 5672, '/', credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('rmq2', 5672, '/', credentials))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
